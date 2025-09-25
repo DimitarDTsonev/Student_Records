ba1564e005744cd115851a34ed03920969c2fe84
Dimitar Tsonev <56803540+DimitarDTsonev@users.noreply.github.com>
2025-09-26 02:06:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -151,4 +151,4 @@ def students():
                            students=all_students)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
